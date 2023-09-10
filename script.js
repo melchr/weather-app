f2359bda0bd2c30ce4dbec9d5199459101112809
Mel C <melchrstmn@gmail.com>
2023-09-10 18:05:45 -0400

    removed console.log

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ let weather = {
         const { icon, description } = data.weather[0]
         const { temp, humidity } = data.main
         const { speed } = data.wind
-        console.log(name,icon,description,temp,humidity,speed)
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon + ".png";
         document.querySelector(".description").innerText = description;
