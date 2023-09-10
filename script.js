ed832187ab71316a08f2c422bfd35a294840aae1
Mel C <melchrstmn@gmail.com>
2023-09-10 17:28:12 -0400

    added weather API functionality

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 let weather = {
-    "apiKey": "6a342d639339a58ef951b69b3baaf208"
+    "apiKey": "6a342d639339a58ef951b69b3baaf208",
+    fetchWeather: function() {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=Denver&units=metric&appid=6a342d639339a58ef951b69b3baaf208").then((response) => response.json())
+        .then((data) => console.log(data))
+    }
 }
