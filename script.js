52d6c38584b95f4035789af1819e6ac7e946432e
Mel C <melchrstmn@gmail.com>
2023-09-10 17:34:27 -0400

    finishing up API functionality

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let weather = {
     "apiKey": "6a342d639339a58ef951b69b3baaf208",
-    fetchWeather: function() {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=Denver&units=metric&appid=6a342d639339a58ef951b69b3baaf208").then((response) => response.json())
+    fetchWeather: function(city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey).then((response) => response.json())
         .then((data) => console.log(data))
+    },
+    displayWeather: function(data) {
+
     }
 }
